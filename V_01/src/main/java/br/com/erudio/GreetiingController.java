9b58111a20007c10c5aa286daa3cf682bdddc3fe
Raimundo Lenildo <rlenildo@info.ufrn.br>
2021-03-27 18:52:17 -0300

    Criação de operações matemáticas básicas

--- V_01/src/main/java/br/com/erudio/GreetiingController.java
+++ V_01/src/main/java/br/com/erudio/GreetiingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetiongController {
+public class GreetiingController {
 
     private static final String template = "Hello, %s";
 
---
+++ V_02_calculadora/src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,76 @@
+package br.com.erudio;
+
+import br.com.erudio.exception.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value = "/sum/{x}/{y}", method = RequestMethod.GET)
+    public Double sum(@PathVariable(value="x") String x, @PathVariable(value = "y") String y) {
+        if(!isNumero(x) || !isNumero(y)){
+            throw new UnsupportedMathOperationException("Por favor, adicione um valor numérico.");
+        }
+        return convertToDouble(x) + convertToDouble(y);
+    }
+
+    @RequestMapping(value = "/sub/{x}/{y}", method = RequestMethod.GET)
+    public Double sub(@PathVariable(value="x") String x, @PathVariable(value = "y") String y) {
+        if(!isNumero(x) || !isNumero(y)){
+            throw new UnsupportedMathOperationException("Por favor, adicione um valor numérico.");
+        }
+        return convertToDouble(x) - convertToDouble(y);
+    }
+
+    @RequestMapping(value = "/mult/{x}/{y}", method = RequestMethod.GET)
+    public Double mult(@PathVariable(value="x") String x, @PathVariable(value = "y") String y) {
+        if(!isNumero(x) || !isNumero(y)){
+            throw new UnsupportedMathOperationException("Por favor, adicione um valor numérico.");
+        }
+        return convertToDouble(x) * convertToDouble(y);
+    }
+
+    @RequestMapping(value = "/div/{x}/{y}", method = RequestMethod.GET)
+    public Double div(@PathVariable(value="x") String x, @PathVariable(value = "y") String y) {
+        if(!isNumero(x) || !isNumero(y)){
+            throw new UnsupportedMathOperationException("Por favor, adicione um valor numérico.");
+        }
+        return convertToDouble(x) / convertToDouble(y);
+    }
+
+    @RequestMapping(value = "/ave/{x}/{y}", method = RequestMethod.GET)
+    public Double ave(@PathVariable(value="x") String x, @PathVariable(value = "y") String y) {
+        if(!isNumero(x) || !isNumero(y)){
+            throw new UnsupportedMathOperationException("Por favor, adicione um valor numérico.");
+        }
+        return (convertToDouble(x) + convertToDouble(y)) / 2;
+    }
+
+    @RequestMapping(value = "/sqrt/{x}", method = RequestMethod.GET)
+    public Double sqrt(@PathVariable(value="x") String x) {
+        if(!isNumero(x)){
+            throw new UnsupportedMathOperationException("Por favor, adicione um valor numérico.");
+        }
+        return (Double) Math.sqrt(convertToDouble(x));
+    }
+
+    private Double convertToDouble(String numero){
+        if(numero != null){
+            if(numero.contains(",")){
+                numero = numero.replaceAll(",",".");
+            }
+            return Double.parseDouble(numero);
+        }
+        return 0D;
+    }
+
+    private boolean isNumero(String numero){
+        if(numero != null){
+            if(numero.contains(",")){
+                numero = numero.replaceAll(",",".");
+            }
+            return numero.matches("[-+]?[0-9]*\\.?[0-9]");
+        }
+        return false;
+    }
+}
